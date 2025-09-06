9af0484b1768abc4a64a234d6bbd9b8b212e9225
Steven Zimmerman, CPA <15812269+EffortlessSteven@users.noreply.github.com>
2025-09-06 19:49:59 -0400

    Restore SwiftMTP app files that were accidentally deleted by DocC generator

---
+++ SwiftMTP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftMTP
+//
+//  Created by Steven Zimmerman on 9/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, SwiftMTP!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
